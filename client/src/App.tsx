5d51ca7863b751be700f1d390f6b6f4ef8a76621
gs-berlin <37944978-gs-berlin@users.noreply.replit.com>
2026-02-18 17:44:03 +0000

    Restored to 'a3798e948e557b0af4f705a0525024019c48f89c'
    Replit-Restored-To: a3798e948e557b0af4f705a0525024019c48f89c

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,6 @@ import { LanguageProvider } from "@/i18n/LanguageContext";
 import NotFound from "@/pages/not-found";
 import { ExtensionMockup } from "@/pages/extension-mockup";
 import { Privacy } from "@/pages/privacy";
-import { StatsDashboard } from "@/pages/stats-dashboard";
 
 function Router() {
   return (
@@ -19,13 +18,9 @@ function Router() {
       <Route path="/es/privacy" component={Privacy} />
       <Route path="/fr/privacy" component={Privacy} />
       <Route path="/de/privacy" component={Privacy} />
-      <Route path="/es/stats" component={StatsDashboard} />
-      <Route path="/fr/stats" component={StatsDashboard} />
-      <Route path="/de/stats" component={StatsDashboard} />
       {/* Default English routes */}
       <Route path="/" component={ExtensionMockup} />
       <Route path="/privacy" component={Privacy} />
-      <Route path="/stats" component={StatsDashboard} />
       <Route component={NotFound} />
     </Switch>
   );
